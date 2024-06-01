45a32580099498276a6faa352b65fc8f7745b3ee
Sourav Bhattacharya <sav.accharya@gmail.com>
2024-06-02 00:06:25 +0100

    Initial commit

---
+++ chatbot/app.py
@@ -0,0 +1,37 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+import streamlit as st
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+os.environ["OPENAI_API_KEY"]=os.getenv("OPENAI_API_KEY")
+
+#Langsmith tracking
+os.environ["LANGCHAIN_TRACING_V2"]="true"
+os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
+
+prompt=ChatPromptTemplate.from_messages(
+    [
+        ("system","You are a helpful assistant. Please response to the user queries"),
+        ("user","Question:{question}")
+    ]
+)
+
+## streamlite framework
+
+st.title('Langchain Demo With OpenAI API')
+input_text = st.text_input("Search the topic you want")
+
+
+#openAI LLM
+
+llm=ChatOpenAI(model="gpt-3.5-turbo")
+output_parser=StrOutputParser()
+chain=prompt|llm|output_parser
+
+if input_text:
+    st.write(chain.invoke({'question': input_text}))
